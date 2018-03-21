5a91ec3313ab16226a524f75db32ca62c41d94d7
Tran Đăng <superluyx@gmail.com>
2018-03-21 14:17:34 +0700

    [UPDATE] goBack

--- src/TabViewPagerExperimental.js
+++ src/TabViewPagerExperimental.js
@@ -18,184 +18,180 @@ const DefaultTransitionSpec = {
   friction: 25,
 };
 
-export default class TabViewPagerExperimental<T: *> extends React.Component<
-  Props<T>
+export default class TabViewPagerExperimental<T: *> extends React.Component <
+  Props < T >
 > {
-  static propTypes = {
-    ...PagerRendererPropType,
-    swipeDistanceThreshold: PropTypes.number,
-    swipeVelocityThreshold: PropTypes.number,
-    GestureHandler: PropTypes.object,
-    handleStateChange: PropTypes.func
-  };
+    static propTypes = {
+      ...PagerRendererPropType,
+      swipeDistanceThreshold: PropTypes.number,
+      swipeVelocityThreshold: PropTypes.number,
+      GestureHandler: PropTypes.object,
+      handleStateChange: PropTypes.func
+    };
 
-  static defaultProps = {
-    GestureHandler:
-      global.__expo && global.__expo.DangerZone
-        ? global.__expo.DangerZone.GestureHandler
-        : undefined,
-    canJumpToTab: () => true,
-  };
+    static defaultProps = {
+      GestureHandler:
+        global.__expo && global.__expo.DangerZone
+          ? global.__expo.DangerZone.GestureHandler
+          : undefined,
+      canJumpToTab: () => true,
+    };
 
-  componentDidUpdate(prevProps: Props<T>) {
-    if (prevProps.navigationState.index !== this.props.navigationState.index) {
-      this._transitionTo(this.props.navigationState.index);
+    componentDidUpdate(prevProps: Props<T>) {
+      if (prevProps.navigationState.index !== this.props.navigationState.index) {
+        this._transitionTo(this.props.navigationState.index);
+      }
     }
-  }
 
   _handleHandlerStateChange = event => {
-    if (typeof this.props.handleStateChange === 'function') {
-      this.props.handleStateChange(event);
-    }
+      if (typeof this.props.handleStateChange === 'function') {
+        this.props.handleStateChange(event);
+      }
 
-    const { GestureHandler } = this.props;
+      const { GestureHandler } = this.props;
 
-    if (event.nativeEvent.state === GestureHandler.State.END) {
-      const {
-        navigationState,
-        layout,
-        swipeDistanceThreshold = layout.width / 1.75,
-        swipeVelocityThreshold = 150,
-      } = this.props;
-      const {
-        translationX,
-        translationY,
-        velocityX,
-        velocityY,
-      } = event.nativeEvent;
-      const currentIndex =
-        typeof this._pendingIndex === 'number'
-          ? this._pendingIndex
-          : navigationState.index;
+      if (event.nativeEvent.state === GestureHandler.State.END) {
+        const {
+          navigationState,
+          layout,
+          swipeDistanceThreshold = layout.width / 1.75,
+          swipeVelocityThreshold = 150,
+        } = this.props;
+        const {
+          translationX,
+          translationY,
+          velocityX,
+          velocityY,
+        } = event.nativeEvent;
+        const currentIndex =
+          typeof this._pendingIndex === 'number'
+            ? this._pendingIndex
+            : navigationState.index;
 
-      let nextIndex = currentIndex;
+        let nextIndex = currentIndex;
 
-      if (
-        translationX > 0
-      ) {
-        if (typeof this.props.goBack === 'function') {
-          this.props.goBack();
+        if (
+          Math.abs(translationX) > Math.abs(translationY) &&
+          Math.abs(velocityX) > Math.abs(velocityY) &&
+          (Math.abs(translationX) > swipeDistanceThreshold ||
+            Math.abs(velocityX) > swipeVelocityThreshold)
+        ) {
+          nextIndex = Math.round(
+            Math.min(
+              Math.max(0, currentIndex - translationX / Math.abs(translationX)),
+              navigationState.routes.length - 1
+            )
+          );
+          if (nextIndex < currentIndex) {
+            this.props.goBack()
+            return
+          }
+          console.log('next')
         }
-        return
+
+        if (
+          !isFinite(nextIndex) ||
+          !this.props.canJumpToTab(this.props.navigationState.routes[nextIndex])
+        ) {
+          nextIndex = currentIndex;
+        }
+
+        this._transitionTo(nextIndex, velocityX);
+      }
+    };
+
+    _transitionTo = (index: number, velocity?: number) => {
+      const offset = -index * this.props.layout.width;
+
+      if (this.props.animationEnabled === false) {
+        this.props.panX.setValue(0);
+        this.props.offsetX.setValue(offset);
+        return;
       }
 
-      if (
-        Math.abs(translationX) > Math.abs(translationY) &&
-        Math.abs(velocityX) > Math.abs(velocityY) &&
-        (Math.abs(translationX) > swipeDistanceThreshold ||
-          Math.abs(velocityX) > swipeVelocityThreshold)
-      ) {
-        nextIndex = Math.round(
-          Math.min(
-            Math.max(0, currentIndex - translationX / Math.abs(translationX)),
-            navigationState.routes.length - 1
-          )
-        );
-      }
+      const { timing, ...transitionConfig } = DefaultTransitionSpec;
+      const { useNativeDriver } = this.props;
 
-      if (
-        !isFinite(nextIndex) ||
-        !this.props.canJumpToTab(this.props.navigationState.routes[nextIndex])
-      ) {
-        nextIndex = currentIndex;
-      }
+      Animated.parallel([
+        timing(this.props.panX, {
+          ...transitionConfig,
+          toValue: 0,
+          velocity,
+          useNativeDriver,
+        }),
+        timing(this.props.offsetX, {
+          ...transitionConfig,
+          toValue: offset,
+          velocity,
+          useNativeDriver,
+        }),
+      ]).start(({ finished }) => {
+        if (finished) {
+          this.props.jumpToIndex(index);
+          this._pendingIndex = null;
+        }
+      });
 
-      this._transitionTo(nextIndex, velocityX);
-    }
-  };
+      this._pendingIndex = index;
+    };
 
-  _transitionTo = (index: number, velocity?: number) => {
-    const offset = -index * this.props.layout.width;
+    _pendingIndex: ? number;
 
-    if (this.props.animationEnabled === false) {
-      this.props.panX.setValue(0);
-      this.props.offsetX.setValue(offset);
-      return;
-    }
+    render() {
+      const {
+        GestureHandler,
+        panX,
+        offsetX,
+        layout,
+        navigationState,
+        swipeEnabled,
+        children,
+      } = this.props;
+      const { width } = layout;
+      const { routes } = navigationState;
+      const maxTranslate = width * (routes.length - 1);
+      const translateX = Animated.add(panX, offsetX).interpolate({
+        inputRange: [-maxTranslate, 0],
+        outputRange: [-maxTranslate, 0],
+        extrapolate: 'clamp',
+      });
 
-    const { timing, ...transitionConfig } = DefaultTransitionSpec;
-    const { useNativeDriver } = this.props;
-
-    Animated.parallel([
-      timing(this.props.panX, {
-        ...transitionConfig,
-        toValue: 0,
-        velocity,
-        useNativeDriver,
-      }),
-      timing(this.props.offsetX, {
-        ...transitionConfig,
-        toValue: offset,
-        velocity,
-        useNativeDriver,
-      }),
-    ]).start(({ finished }) => {
-      if (finished) {
-        this.props.jumpToIndex(index);
-        this._pendingIndex = null;
-      }
-    });
-
-    this._pendingIndex = index;
-  };
-
-  _pendingIndex: ?number;
-
-  render() {
-    const {
-      GestureHandler,
-      panX,
-      offsetX,
-      layout,
-      navigationState,
-      swipeEnabled,
-      children,
-    } = this.props;
-    const { width } = layout;
-    const { routes } = navigationState;
-    const maxTranslate = width * (routes.length - 1);
-    const translateX = Animated.add(panX, offsetX).interpolate({
-      inputRange: [-maxTranslate, 0],
-      outputRange: [-maxTranslate, 0],
-      extrapolate: 'clamp',
-    });
-
-    return (
-      <GestureHandler.PanGestureHandler
-        enabled={layout.width !== 0 && swipeEnabled !== false}
-        minDeltaX={10}
-        onGestureEvent={Animated.event(
-          [{ nativeEvent: { translationX: this.props.panX } }],
-          { useNativeDriver: this.props.useNativeDriver }
-        )}
-        onHandlerStateChange={this._handleHandlerStateChange}
-      >
-        <Animated.View
-          style={[
-            styles.sheet,
-            width
-              ? { width: routes.length * width, transform: [{ translateX }] }
-              : null,
-          ]}
+      return (
+        <GestureHandler.PanGestureHandler
+          enabled={layout.width !== 0 && swipeEnabled !== false}
+          minDeltaX={10}
+          onGestureEvent={Animated.event(
+            [{ nativeEvent: { translationX: this.props.panX } }],
+            { useNativeDriver: this.props.useNativeDriver }
+          )}
+          onHandlerStateChange={this._handleHandlerStateChange}
         >
-          {React.Children.map(children, (child, i) => (
-            <View
-              key={navigationState.routes[i].key}
-              testID={navigationState.routes[i].testID}
-              style={
-                width
-                  ? { width }
-                  : i === navigationState.index ? StyleSheet.absoluteFill : null
-              }
-            >
-              {i === navigationState.index || width ? child : null}
-            </View>
-          ))}
-        </Animated.View>
-      </GestureHandler.PanGestureHandler>
-    );
+          <Animated.View
+            style={[
+              styles.sheet,
+              width
+                ? { width: routes.length * width, transform: [{ translateX }] }
+                : null,
+            ]}
+          >
+            {React.Children.map(children, (child, i) => (
+              <View
+                key={navigationState.routes[i].key}
+                testID={navigationState.routes[i].testID}
+                style={
+                  width
+                    ? { width }
+                    : i === navigationState.index ? StyleSheet.absoluteFill : null
+                }
+              >
+                {i === navigationState.index || width ? child : null}
+              </View>
+            ))}
+          </Animated.View>
+        </GestureHandler.PanGestureHandler>
+      );
+    }
   }
-}
 
 const styles = StyleSheet.create({
   sheet: {
